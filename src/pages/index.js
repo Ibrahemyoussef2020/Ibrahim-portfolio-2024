67c3c3f447e3e396213796913a9292c9a23f88b2
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-05-02 14:18:45 +0400

    fix prject sec error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,4 @@ export {default as Home} from './Home';
 export {default as About} from './About';
 export {default as Contact} from './Contact';
 export {default as Challenges} from './Challenges';
-export {default as Projects} from './Projects';
+export {default as Projects} from './projects';
