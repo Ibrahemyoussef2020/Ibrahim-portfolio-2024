df6c6bbf55560314f133b90b5d2a3a702a09a01e
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2025-02-02 00:40:16 +0200

    ammet

--- src/components/general/SocialBar.jsx
+++ src/components/general/SocialBar.jsx
@@ -6,13 +6,13 @@ const SocialBar = () => {
         <a className='facebook' href="https://web.facebook.com/facebook.comibrahimyoussef.saleh/?locale=ar_AR" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-facebook-f"></i>
         </a>
-        <a className='linkedin' href="https://web.facebook.com/facebook.comibrahimyoussef.saleh/?locale=ar_AR" target="_blank" rel="noopener noreferrer">
+        <a className='linkedin' href="linkedin.com/in/ibrahim-youssef-2a65b1261" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-linkedin-in"></i>
         </a>
-        <a className='github' href="https://web.facebook.com/facebook.comibrahimyoussef.saleh/?locale=ar_AR" target="_blank" rel="noopener noreferrer">
+        <a className='github' href="https://github.com/Ibrahemyoussef2020" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-github"></i>
         </a>
-        <a className='whatsapp' href="https://web.facebook.com/facebook.comibrahimyoussef.saleh/?locale=ar_AR" target="_blank" rel="noopener noreferrer">
+        <a className='whatsapp' href="https://wa.me/01147359396" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-whatsapp"></i>
         </a>
     </div>
